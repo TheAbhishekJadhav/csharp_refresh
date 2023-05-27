ff118917a8bae0d9b93e79a63692ac20bc051425
Abhishek Jadhav <89737249+TheAbhishekJadhav@users.noreply.github.com>
2023-05-27 12:12:07 +0200

    second commit

---
+++ DataTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DataTypes;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Abhishek Jadhav";
+        char initial = 'A';
+        int age = 30;
+        // float gpa1 = 3.3;
+        double gpa2 = 3.3;
+        // decimal gpa3 = 3.3; // for precise decimal operations 
+        bool isMale = true;
+        Console.WriteLine(name + " " + age + " " + gpa2);
+    }
+}
---
+++ ExponentMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ExponentMethod;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(GetPow(4,2));
+    }
+
+    static int GetPow(int baseNum, int powNum)
+    {
+        int result = 1;
+
+        for(int i = 0; i < powNum; i++)
+        {
+            result = result * baseNum;
+        }        
+        return result;
+    }
+}
