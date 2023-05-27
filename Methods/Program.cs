7d77f69c39da680a0c3114ab64d55992e45fd192
Abhishek Jadhav <89737249+TheAbhishekJadhav@users.noreply.github.com>
2023-05-27 12:22:51 +0200

    sixth commit

---
+++ Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Methods;
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayHi("Mike", 38);
+        SayHi("John", 57);
+        SayHi("Tom", 13);
+        Console.WriteLine(Cube(5));
+    }
+
+    static void SayHi(string name, int age)
+    {
+        Console.WriteLine("Hello " + name + " you are " + age);
+    }
+
+    static int Cube(int num)
+    {
+        int result = num * num * num;
+        return result;
+    }
+}
---
+++ Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Numbers;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = 6;
+        num++;
+        Console.WriteLine(12);
+        Console.WriteLine( 5 % 2 );
+        Console.WriteLine( ( 4 + 2 ) * 3 );
+        Console.WriteLine( 5 / 2.0 );
+        Console.WriteLine(num);
+        Console.WriteLine(Math.Abs(-40));
+        Console.WriteLine(Math.Pow(3, 2));
+        Console.WriteLine(Math.Sqrt(69));
+        Console.WriteLine(Math.Max(2, 40));
+        Console.WriteLine(Math.Round(4.6));
+    }
+}
