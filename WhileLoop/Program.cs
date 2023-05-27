887e16434dab783885c6438f303ec1e5070b9407
Abhishek Jadhav <89737249+TheAbhishekJadhav@users.noreply.github.com>
2023-05-27 12:38:59 +0200

    last commit

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace WhileLoop;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int index = 0;
+        do 
+        {
+            Console.WriteLine(index);
+            index++;
+        } while(index<=5);
+        
+    }
+}
