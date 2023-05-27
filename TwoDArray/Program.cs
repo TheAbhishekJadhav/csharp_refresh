d7ea28ddf29815e2a564d6fdd501a3e0c95c14fb
Abhishek Jadhav <89737249+TheAbhishekJadhav@users.noreply.github.com>
2023-05-27 12:37:26 +0200

    tenth commit

---
+++ TwoDArray/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TwoDArray;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] numberGrid = {
+            {1, 2},
+            {3, 4},
+            {5, 6}
+        };
+        Console.WriteLine(numberGrid[2,1]);
+    }
+}
---
+++ Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Variables;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string charecterName = "Tom";
+        int characterAge = 50;
+        Console.WriteLine("There once was a man named " + charecterName);
+        Console.WriteLine("He was " + characterAge + " years old");
+
+        charecterName = "Mike";
+        Console.WriteLine("He really liked the name " + charecterName);
+        Console.WriteLine("But didn't like being " + characterAge);
+    }
+}
