71b3a8a130f2961aa336787576bee4a02fb1842b
Abhishek Jadhav <89737249+TheAbhishekJadhav@users.noreply.github.com>
2023-05-27 12:18:26 +0200

    fourth commit

---
+++ Comments/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Comments;
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        this is multiline comment
+        */
+        Console.WriteLine("Comments are fun"); // execute this
+        // this is single line comment
+    }
+}
---
+++ ForLoop/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ForLoop;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] luckyNumber = {2, 8, 14, 26, 35, 57};
+
+        for(int i = 0; i < luckyNumber.Length; i++)
+            Console.WriteLine(luckyNumber[i]);
+    }
+}
